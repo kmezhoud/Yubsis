294dee82ca74e92f8606843200d77be5bc979953
Karim Mezhoud <kmezhoud@gmail.com>
2020-08-11 00:22:09 +0100

    oooo

--- generate_DNA.py
+++ generate_DNA.py
@@ -7,4 +7,4 @@ repeat10 =  'tcatg' * 2
 repeat12 = 'tatg' * 3
 
 def generate_DNA(N, alphabet=['A','C','G','T',repeat4,repeat10, repeat12]):
-    return(''.join([random.choice(alphabet) for i in range(N)]))
+  return(''.join([random.choice(alphabet) for i in range(N)]))
