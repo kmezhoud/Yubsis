48c5fa74317b41733f35f7a05265a58b8ea7c29a
Karim Mezhoud <kmezhoud@gmail.com>
2020-08-11 00:32:18 +0100

    add /src

---
+++ src/generate_DNA.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import random
+
+repeat4 = 'atca'  
+repeat10 =  'tcatg' * 2 
+repeat12 = 'tatg' * 3
+
+   # """
+   #      * Description: generate DNA sequence from ATCG and reapeats
+   #      * N: Is the number of random sampling
+   #      * alphabet: is a list of ['A','C','G','T',repeat4,repeat10, repeat12]
+   #      * return: DNA sequence with upper and lower cases
+   # """
+
+def generate_DNA(N, alphabet=['A','C','G','T',repeat4,repeat10, repeat12]):
+  return(''.join([random.choice(alphabet) for i in range(N)]))
